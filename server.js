348d607b19b8aae16809710763a7c4c334dec7c2
xylo <xylohunter1991@gmail.com>
2024-01-21 10:47:22 +0100

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express') 
+const PORT = process.env.PORT || 3000
 const app = express() 
 app.get('/api',(req,res)=>{ 
     res.send(`<h5 style="color:green"> 
         Hey Geek! you just deployed serverless express api</h5>`) 
 }) 
-app.listen(8080,()=>{ 
+app.listen(PORT,()=>{ 
     console.log('Server started at http://localhost:8080') 
 }) 
 module.exports=app
